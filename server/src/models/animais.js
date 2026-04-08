26925b431d47d8dcaf16653dee3da641948c27b8
Daniel Oliveira Araújo <danieloliveira14@gmail.com>
2026-04-08 14:27:14 -0300

    corrigindo ortografias

--- server/src/models/animais.js
+++ server/src/models/animais.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const formatarTextoPtBr = require("../utils/formatarTextoPtBr");
 module.exports = (sequelize, DataTypes) => {
   class animais extends Model {
     /**
@@ -10,14 +9,45 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      animais.hasMany(models.lote, { foreignKey: 'animal_id', as: 'ass_animal_lote' })
+      animais.hasMany(models.lote, {
+        foreignKey: "animal_id",
+        as: "ass_animal_lote",
+      });
     }
   }
-  animais.init({
-    tipo: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'animais',
-  });
+  animais.init(
+    {
+      tipo: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "animais",
+      // ✅ HOOKS NO LUGAR CERTO
+      hooks: {
+        beforeCreate: (instance) => {
+          formatarCamposString(instance);
+        },
+        beforeUpdate: (instance) => {
+          formatarCamposString(instance);
+        },
+      },
+    },
+  );
   return animais;
 };
+
+// const CAMPOS_EXCLUIDOS = [
+//   'pedido', 'regime_cultivo'
+// ];
+
+// 🔹 Função genérica: percorre TODOS os campos STRING
+function formatarCamposString(instance) {
+  Object.keys(instance.dataValues).forEach((campo) => {
+    
+    const valor = instance.dataValues[campo];
+
+    if (typeof valor === "string") {
+      instance.dataValues[campo] = formatarTextoPtBr(valor.trim());
+    }
+  });
+}
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const formatarTextoPtBr = require("../utils/formatarTextoPtBr");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -10,22 +9,62 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      user.belongsTo(models.empresa, { foreignKey: 'empresa_id', as: 'ass_user_empresa' });
-      user.belongsTo(models.perfil, { foreignKey: 'perfil_id', as: 'ass_user_perfil' });
-      user.hasMany(models.audit, { foreignKey: 'user_id', as: 'ass_user_audit' });  
+      user.belongsTo(models.empresa, {
+        foreignKey: "empresa_id",
+        as: "ass_user_empresa",
+      });
+      user.belongsTo(models.perfil, {
+        foreignKey: "perfil_id",
+        as: "ass_user_perfil",
+      });
+      user.hasMany(models.audit, {
+        foreignKey: "user_id",
+        as: "ass_user_audit",
+      });
     }
   }
-  user.init({
-    nome: DataTypes.STRING,
-    user_name: DataTypes.STRING,
-    user_email: DataTypes.STRING,
-    user_active: DataTypes.BOOLEAN,
-    user_password: DataTypes.STRING,
-    user_pin: DataTypes.STRING,
-    cpf: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  user.init(
+    {
+      nome: DataTypes.STRING,
+      user_name: DataTypes.STRING,
+      user_email: DataTypes.STRING,
+      user_active: DataTypes.BOOLEAN,
+      user_password: DataTypes.STRING,
+      user_pin: DataTypes.STRING,
+      cpf: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "user",
+      // ✅ HOOKS NO LUGAR CERTO
+      hooks: {
+        beforeCreate: (instance) => {
+          formatarCamposString(instance);
+        },
+        beforeUpdate: (instance) => {
+          formatarCamposString(instance);
+        },
+      },
+    },
+  );
   return user;
 };
+
+const CAMPOS_EXCLUIDOS = [
+  'user_password', 'user_email'
+];
+
+// 🔹 Função genérica: percorre TODOS os campos STRING
+function formatarCamposString(instance) {
+  Object.keys(instance.dataValues).forEach((campo) => {
+
+    // ⛔ pula campos excluídos
+    if (CAMPOS_EXCLUIDOS.includes(campo)) return;
+
+    const valor = instance.dataValues[campo];
+
+    if (typeof valor === 'string') {
+      instance.dataValues[campo] = formatarTextoPtBr(valor.trim());
+    }
+  });
+}
