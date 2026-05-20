251948de565b75541ae0d80eba2475de6753e1f3
Daniel Oliveira Araújo <danieloliveira14@gmail.com>
2026-05-20 20:38:23 -0300

    criando seeds

---
+++ server/src/seeders/20260520225257-user.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          empresa_id: 1,
+          nome: "Daniel Oliveira Araújo",
+          user_name: "Daniel Araújo",
+          user_email: "danieloliveira14@gmail.com",
+          user_active: true,
+          password_hash: "$2b$10$9e8T/ZbCDB388lMyc4E/1OVsUNewvFS8vwKEqhI5OFCVPo/eH9G6O",
+          user_pin: "2349",
+          cpf: "93307390325",
+          profile_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
---
+++ server/src/seeders/20260520225528-client_company.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "empresas",
+      [
+        {
+          nome: "Fazenda Sistema",
+          cnpj: "12536589000125",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
